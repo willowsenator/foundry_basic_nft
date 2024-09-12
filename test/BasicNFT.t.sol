36e2102aab19f288e70134053e14ee6a9cf94b58
willowsenator <willowsenator@gmail.com>
2024-09-13 01:57:06 +0200

    Add tests and interactions with NFT

---
+++ test/BasicNFT.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNFT.s.sol";
+
+contract BasicNFTTest is Test {
+    DeployBasicNFT public deployer;
+    BasicNFT public basicNFT;
+    address public USER = makeAddr("USER");
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        basicNFT = deployer.run();
+    }
+
+    function testNameIsCorrect() public view{
+        assert(keccak256(abi.encodePacked(basicNFT.name())) == keccak256(abi.encodePacked("BasicNFT")));
+    }
+
+    function testSymbolIsCorrect() public view{
+        assert(keccak256(abi.encodePacked(basicNFT.symbol())) == keccak256(abi.encodePacked("BNFT")));
+    }
+
+    function testCanMintAndHaveBalance() public { 
+        vm.prank(USER);
+        basicNFT.mintNFT(PUG);
+
+        assert(basicNFT.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNFT.tokenURI(0))) == keccak256(abi.encodePacked(PUG)));
+    }
+}
