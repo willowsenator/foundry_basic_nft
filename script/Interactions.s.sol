700523710ec467e82fd8bbe8d7f44581a5895342
willowsenator <willowsenator@gmail.com>
2024-10-12 18:49:23 +0200

    modules

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity 0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
--- test/BasicNFTTest.t.sol
+++ test/BasicNFTTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity 0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
